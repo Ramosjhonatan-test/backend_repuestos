99d89af8f1252d214e1e92f336079a2a741cc29d
Ramosjhonatan-test <ramosjhonatan659@gmail.com>
2026-04-07 13:30:36 -0400

    Primer despliegue V 1.0

--- api/index.js
+++ api/index.js
@@ -10,19 +10,8 @@ const app = require('../src/app');
 const { sequelize } = require('../src/models');
 
 module.exports = async (req, res) => {
-  // Configuración manual de CORS para Vercel
-  res.setHeader('Access-Control-Allow-Origin', 'https://repuestos-web-five.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
   try {
     // Verificar conexión a la DB (Neon)
-    // Usamos authenticate() pero sin bloquear demasiado el arranque serverless
     await sequelize.authenticate();
     
     // Delegar la petición a Express
